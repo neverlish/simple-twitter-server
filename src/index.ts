37fc7aaa7a29d6d5dc9ed6e5351318cec6686a0c
Jinho Hyeon <neverlish@gmail.com>
2022-04-06 08:30:13 +0900

    graphql 기본 설정

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
 import express from 'express';
+import { graphqlHTTP } from 'express-graphql';
+import { makeExecutableSchema } from 'graphql-tools'
+import typeDefs from './typeDefs';
+import resolvers from './resolvers';
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+});
 
 const app = express();
 
-app.get('/', (req, res) => res.send('hello world'));
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: true,
+}));
 
 app.listen(4000, () => console.log('server started'));
